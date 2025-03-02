2c98cd1e6584ba5eef4e539301d91a06b1d3a23c
Chinseok Lee <me@pyhub.kr>
2025-03-02 17:35:49 +0900

    임베딩 모델에 대한 차원수 주석 추가

--- src/pyhub/rag/llm.py
+++ src/pyhub/rag/llm.py
@@ -17,12 +17,14 @@ from typing_extensions import TypeAlias
 from pyhub.rag.settings import rag_settings
 
 
-OpenAIEmbeddingModel: TypeAlias = Union[
-    Literal["text-embedding-ada-002", "text-embedding-3-small", "text-embedding-3-large"],
+OpenAIEmbeddingModel: TypeAlias = Literal[
+    "text-embedding-ada-002",  # 1536 차원
+    "text-embedding-3-small",  # 1536 차원
+    "text-embedding-3-large",  # 3072 차원
 ]
 
 # https://cloud.google.com/vertex-ai/generative-ai/docs/embeddings/get-text-embeddings?hl=ko
-GoogleEmbeddingModel: TypeAlias = Union[Literal["text-embedding-004"]]  # 768 차원
+GoogleEmbeddingModel: TypeAlias = Literal["text-embedding-004"]  # 768 차원
 
 LLMEmbeddingModel = Union[OpenAIEmbeddingModel, GoogleEmbeddingModel]
 
