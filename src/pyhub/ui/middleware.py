dd7b7f282bab422ca6f0cf158e23af81f87c15f3
Chinseok Lee <me@pyhub.kr>
2025-04-02 22:46:14 +0900

    TimezoneMiddleware에 async 지원 추가

--- src/pyhub/ui/middleware.py
+++ src/pyhub/ui/middleware.py
@@ -1,5 +1,6 @@
 import zoneinfo
 
+from asgiref.sync import iscoroutinefunction, markcoroutinefunction
 from django.conf import settings
 from django.utils import timezone
 
@@ -7,18 +8,42 @@ from django.utils import timezone
 
 
 class TimezoneMiddleware:
+    sync_capable = True
+    async_capable = True
+
     def __init__(self, get_response):
+        if get_response is None:
+            raise ValueError("get_response must be provided.")
         self.get_response = get_response
+        self.is_async = iscoroutinefunction(get_response)
+        if self.is_async:
+            markcoroutinefunction(self)
+        super().__init__()
 
     def __call__(self, request):
+        if self.is_async:
+            return self.__acall__(request)
+
+        self._process_request(request)
+        return self.get_response(request)
+
+    async def __acall__(self, request):
+        self._process_request(request)
+        return await self.get_response(request)
+
+    def _process_request(self, request):
+        """Shared synchronous logic to process the request."""
         tzname = request.session.get("django_timezone")
         if not tzname:
             if hasattr(settings, "USER_DEFAULT_TIME_ZONE"):
                 tzname = settings.USER_DEFAULT_TIME_ZONE
 
         if tzname:
-            timezone.activate(zoneinfo.ZoneInfo(tzname))
+            try:
+                timezone.activate(zoneinfo.ZoneInfo(tzname))
+            except zoneinfo.ZoneInfoNotFoundError:
+                timezone.deactivate()
         else:
+            # If no timezone is found in session or default setting, deactivate
+            # to use the default (settings.TIME_ZONE)
             timezone.deactivate()
-
-        return self.get_response(request)
